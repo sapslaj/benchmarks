fb912f827bb6fc5d7ee25953797c43b4bfbdffa5
sapslaj <saps.laj@gmail.com>
2023-07-01 19:30:10 -0400

    Support docker for nginx thing

--- http-get-hello-world/nginx/start.sh
+++ http-get-hello-world/nginx/start.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-nerdctl run -it --rm -p 8080:8080 -v $(pwd)/nginx.conf:/etc/nginx/conf.d/default.conf nginx
+if command -v nerdctl &>/dev/null ; then
+  nerdctl run -it --rm -p 8080:8080 -v $(pwd)/nginx.conf:/etc/nginx/conf.d/default.conf nginx
+elif command -v docker &>/dev/null ; then
+  docker run -it --rm -p 8080:8080 -v $(pwd)/nginx.conf:/etc/nginx/conf.d/default.conf nginx
+else
+  echo 'no supported way to run'
+  exit 1
+fi
