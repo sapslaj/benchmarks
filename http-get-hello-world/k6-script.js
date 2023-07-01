0480f49ef9fe2bdf080a36043e395c04b119891a
sapslaj <saps.laj@gmail.com>
2023-07-01 19:47:27 -0400

    Add k6 checks

--- http-get-hello-world/k6-script.js
+++ http-get-hello-world/k6-script.js
@@ -1,5 +1,5 @@
 import http from 'k6/http';
-import { sleep } from 'k6';
+import { sleep, check } from 'k6';
 
 export const options = {
   executor: 'ramping-arrival-rate',
@@ -12,7 +12,10 @@ export const options = {
 };
 
 export default function() {
-  http.get('http://localhost:8080');
-
-  sleep(1);
+  const res = http.get('http://localhost:8080');
+  check(res, {
+    'is status 200': (r) => r.status === 200,
+    'verify homepage text': (r) => r.body.includes('hello world'),
+  });
+  sleep(0.1);
 }
